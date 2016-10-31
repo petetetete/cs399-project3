920ba1e6534cd67dcefc0de75bb83ba7098ee67d
Marjorie Hahn <Marjorie Hahn>
2016-10-30 21:41:55 -0700

    Modified camera activity layout.

--- app/src/main/java/android/cs399_project3/NewCameraActivity.java
+++ app/src/main/java/android/cs399_project3/NewCameraActivity.java
@@ -1,7 +1,9 @@
 package android.cs399_project3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewCameraActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class NewCameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_camera);
+
+        findViewById(R.id.AddCameraButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CamerasActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
