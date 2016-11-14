025f72ef24a24b25bd67dad9c660b7b4da3089d2
petetetete <huettlpeter@gmail.com>
2016-11-14 16:46:47 -0700

    Add app settings to save and improve notifications

--- app/src/main/java/android/cs399_project3/MainGlobal.java
+++ app/src/main/java/android/cs399_project3/MainGlobal.java
@@ -1,9 +1,13 @@
 package android.cs399_project3;
 
 import android.app.Application;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 
 import org.json.JSONArray;
@@ -37,10 +41,33 @@ public class MainGlobal extends Application {
 
     public void saveData() {
         try {
+            // Main json object
+            /*{
+                "settings": {
+                    "notifications": boolean
+                },
+                "cameras": [{
+                    "name": String,
+                    "status": int,
+                    "notifications": boolean,
+                    "url": String,
+                    "startTime": String,
+                    "endTime": String,
+                    "log": [String]
+                }]
+            }*/
+
+            JSONObject appData = new JSONObject();
+
+            // Save application settings
+            JSONObject appSettings = new JSONObject();
+            appSettings.put("notifications", settings.getNotifications());
+            appData.put("settings", appSettings);
+
+            // Save camera settings
             JSONArray cameraData = new JSONArray();
             JSONObject camera;
             JSONArray cameraLog;
-
             for (Camera cam : cameras) {
                 camera = new JSONObject();
                 camera.put("name", cam.getName());
@@ -55,12 +82,14 @@ public class MainGlobal extends Application {
 
                 cameraData.put(camera);
             }
+            appData.put("cameras", cameraData);
 
-            String cameraDataString = cameraData.toString();
-
+            // Save json into shared preferences
             SharedPreferences.Editor prefEditor = getSharedPreferences("appData", Context.MODE_PRIVATE).edit();
-            prefEditor.putString("cameras", cameraDataString);
+            prefEditor.putString("cameras", appData.toString());
             prefEditor.apply();
+
+            createNotification("Data saved!", "Camera data has been saved.");
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -69,10 +98,17 @@ public class MainGlobal extends Application {
 
     public void loadData() {
         try {
+            // Get json from shared preferences
             SharedPreferences sharedPref = getSharedPreferences("appData", Context.MODE_PRIVATE);
             String cameraJSON = sharedPref.getString("cameras", "");
+            JSONObject appData = new JSONObject(cameraJSON);
 
-            JSONArray cameraData = new JSONArray(cameraJSON);
+            // Load application settings
+            JSONObject appSettings = appData.getJSONObject("settings");
+            settings.setNotifications(appSettings.getBoolean("notifications"));
+
+            // Load camera data
+            JSONArray cameraData = appData.getJSONArray("cameras");
             for (int i = 0; i < cameraData.length(); i++) {
                 JSONObject camObject = cameraData.getJSONObject(i);
 
@@ -91,19 +127,19 @@ public class MainGlobal extends Application {
 
                 addCamera(camName, camStatus, camNotifications, camUrl, camStartTime, camEndTime);
                 getLatestCamera().setLog(camLog);
+
+                createNotification("Data loaded!", "Camera data has been fetched.");
                 dataLoaded = true;
             }
         }
         catch (Exception e) {
             e.printStackTrace();
 
-            // Load mock data if there is a problem loading data
+            // Load mock data if    there is a problem loading data
             addCamera("Example Camera", 1, true,  "https://ia800309.us.archive.org/2/items/Popeye_Nearlyweds/Popeye_Nearlyweds_512kb.mp4", "4:45", "5:45");
             cameras.get(0).addLogEntry("Camera created.");
-
             addCamera("Test Camera", 0, false, "https://ia600208.us.archive.org/4/items/Popeye_forPresident/Popeye_forPresident_512kb.mp4", "10:20", "14:20");
             cameras.get(1).addLogEntry("Camera created.");
-
             addCamera("Disabled Camera", 2, true, "https://ia902606.us.archive.org/15/items/Popeye_Cooking_With_Gags_1954/Popeye_Cookin_with_Gags_512kb.mp4", "21:38", "23:00");
             cameras.get(2).addLogEntry("Camera created.");
             cameras.get(2).addLogEntry("Camera turned off.");
@@ -114,6 +150,7 @@ public class MainGlobal extends Application {
             cameras.get(2).addLogEntry("Camera enabled.");
             cameras.get(2).addLogEntry("Camera disabled.");
 
+            createNotification("Mock data loaded!", "Loaded example data.");
             dataLoaded = true;
         }
     }
@@ -160,12 +197,22 @@ public class MainGlobal extends Application {
 
     public void createNotification(String title, String message) {
         if (settings.getNotifications()) {
-            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
+            PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(this, MainGlobal.class), 0);
+
+            Notification notification = new NotificationCompat.Builder(getApplicationContext())
+                    .setTicker(title)
                     .setSmallIcon(R.drawable.ic_camera_icon)
                     .setContentTitle(getString(R.string.cameras_activity_name) + ": " + title)
-                    .setContentText(message);
-            nManager.notify(001, mBuilder.build());
+                    .setContentText(message)
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+
+            NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            nManager.notify(0, notification);
+
+            Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(100);
         }
     }
 
